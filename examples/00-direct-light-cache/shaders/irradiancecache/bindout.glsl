e3eca6802d2b5a60f1760917fcadff2aadbdd2a9
Firestar99 <4696087-firestar99@users.noreply.gitlab.com>
2023-05-24 18:03:55 +0200

    ic: make indirect accumulate on indirect ic, not temp ic

--- examples/00-direct-light-cache/shaders/irradiancecache/bindout.glsl
+++ examples/00-direct-light-cache/shaders/irradiancecache/bindout.glsl
@@ -8,10 +8,13 @@
 #define SET_OUT_IRRADIANCE_CACHE 0
 #endif
 
-layout(set = SET_OUT_IRRADIANCE_CACHE, binding = BIND_OUT_IRRADIANCE_CACHE, rgba32f) uniform writeonly image3D IrradianceCacheOut;
+layout(set = SET_OUT_IRRADIANCE_CACHE, binding = BIND_OUT_IRRADIANCE_CACHE, rgba32f) uniform image3D IrradianceCacheOut;
 
 void writeIrradianceCache(uvec3 pixelSpace, vec4 value) {
 	imageStore(IrradianceCacheOut, ivec3(pixelSpace), value);
+}
+vec4 readOutIrradianceCache(uvec3 pixelSpace) {
+	return imageLoad(IrradianceCacheOut, ivec3(pixelSpace));
 }
 
 #undef BIND_OUT_IRRADIANCE_CACHE
